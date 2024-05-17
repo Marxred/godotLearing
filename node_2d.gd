00dfcbd901cb5f06deb6ccedfbc18bf7d7b91661
LAPTOP-NO2B2SJ5\charname <marx>
2024-05-17 20:55:06 +0800

    打印输出

--- node_2d.gd
+++ node_2d.gd
@@ -8,4 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	print(global_position)
+	print(global_rotation)
